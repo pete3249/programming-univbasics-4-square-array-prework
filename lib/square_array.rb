6bb70e2431e511437a375062da0d0dbfe47e01e2
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-30 15:26:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,7 @@
 def square_array(array)
   new_array = []
-  count = 0
-  while count < array.length do
-    
-    
-    
-    new_array.push(array**2)
-    count += 1
+  array.length.times do |index|
+    new_array.push(array[index]**2)
   end
   new_array
-end 
+end
